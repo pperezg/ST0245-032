902eaa981658ebfef14360f8146b345267d29476
pperezg <pperezg@eafit.edu.co>
2019-08-14 20:28:13 -0500

    Grafica de taller 5 punto 2

--- talleres/taller05/Ciclo_List_sum.py
+++ talleres/taller05/Ciclo_List_sum.py
@@ -1,11 +1,25 @@
-import matplotlib.pyplot as plt
-import time
+import matplotlib.pyplot as plt 
+import timeit
+import sys
 
 lista = [4, 2, 5, 7, 3]
-suma = 0
-for i in lista:
-    suma = suma + i
-print(suma)
 
-plt.plot(lista,lista)
+def suma(lista):
+    suma = 0
+    for i in lista:
+        suma = suma + i
+    return suma
+
+a = []
+tiempos = []
+for f in range (1,40000):
+    a.append(f)
+    start_time = timeit.default_timer()
+    suma(a)
+    tiempos.append(timeit.default_timer()-start_time)
+
+plt.plot(a,tiempos,'ro')
+plt.axis([0,40000,0,0.1])
+plt.ylabel('tiempo en segundos')
+plt.xlabel('largo del arreglo')
 plt.show()
