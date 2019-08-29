4ccdd205120c618ac634c618b1919971b5beb978
pperezg <pperezg@eafit.edu.co>
2019-08-28 21:22:55 -0500

    cambio limite

--- laboratorios/lab01/codigo/puertoAntioquia.py
+++ laboratorios/lab01/codigo/puertoAntioquia.py
@@ -8,7 +8,7 @@ def opciones1por2(n):
     else:
         return opciones1por2(n-2) + opciones1por2(n-1)
 
-sys.setrecursionlimit(70)
+sys.setrecursionlimit(70000)
 print(opciones1por2(5))
 
 a = []
@@ -20,9 +20,9 @@ for f in range (1,50):
     tiempos.append((timeit.default_timer()-st)*1000)
 
 plt.plot(a,tiempos,'ro')
-plt.axis([0,50,0,3000])
+plt.axis([0,50,0,10000])
 plt.ylabel('tiempo en milisegundos')
 plt.xlabel('largo del arreglo')
 plt.show()
 
-print(tiempos[49])
+print(tiempos[0])
